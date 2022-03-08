352a7bb2ad1b70eb3f2b31ef2ee179f6c1c93089
hongho092 <93205420+hongho092@users.noreply.github.com>
2022-03-08 14:22:19 +0700

    first commit

---
+++ src/try_1/try1.java
@@ -0,0 +1,16 @@
+package try_1;
+
+public class try1 {
+    public static void main(String[] args) {
+
+        try {
+            int x = 0;
+            int y = 5 / x;
+        }catch (ArithmeticException e) {
+            System.out.println("Arithmetic");
+        }catch (Exception ae) {
+            System.out.println("Exception");
+        }
+        System.out.println("finished");
+    }
+}
